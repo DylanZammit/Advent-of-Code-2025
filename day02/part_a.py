a0b88bcaaaa28183e6be37da49b756474147db41
Dylan Zammit <carbonara@Dylans-MacBook-Air.local>
2025-11-30 22:30:47 +0100

    setup folder and file structure

---
+++ day02/part_a.py
@@ -0,0 +1,11 @@
+from utils import *
+from aocd import get_data, submit
+from itertools import combinations
+
+year, day = 2025, 2
+
+dat = get_data(year=year, day=day, block=True)
+# CODE HERE
+
+print(out)
+# submit(out, part="a", day=day, year=year)
---
+++ day10/part_b.py
@@ -0,0 +1,11 @@
+from utils import *
+from aocd import get_data, submit
+from itertools import combinations
+
+year, day = 2025, 10
+
+dat = get_data(year=year, day=day, block=True)
+# CODE HERE
+
+print(out)
+# submit(out, part="b", day=day, year=year)
