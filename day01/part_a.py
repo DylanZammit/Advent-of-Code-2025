3c2ae15770febac7338a028a419ba25d3b471ae8
carbonara <dylanzam@gmail.com>
2025-12-01 06:34:06 +0100

    day 1 sol

--- day01/part_a.py
+++ day01/part_a.py
@@ -1,11 +1,30 @@
-from utils import *
 from aocd import get_data, submit
-from itertools import combinations
 
 year, day = 2025, 1
 
-dat = get_data(year=year, day=day, block=True)
-# CODE HERE
+dat = '''L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82'''
 
-print(out)
-# submit(out, part="a", day=day, year=year)
+dat = get_data(year=year, day=day, block=True)
+
+dat = dat.split('\n')
+dial = 50
+res = 0
+
+for w in dat:
+    s, d = -1 if w[0] == 'L' else 1, int(w[1:])
+    dial = (dial + s * d) % 100
+    if dial == 0:
+        res += 1
+
+
+print(res)
+submit(res, part="a", day=day, year=year)
--- day01/part_b.py
+++ day01/part_b.py
@@ -1,11 +1,36 @@
-from utils import *
 from aocd import get_data, submit
-from itertools import combinations
 
 year, day = 2025, 1
 
-dat = get_data(year=year, day=day, block=True)
-# CODE HERE
+dat = '''L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82'''
 
-print(out)
-# submit(out, part="b", day=day, year=year)
+dat = get_data(year=year, day=day, block=True)
+
+dat = dat.split('\n')
+dial = 50
+res = 0
+
+for w in dat:
+    s, d = w[0], int(w[1:])
+    s = -1 if s == 'L' else 1
+
+    res += d // 100
+    rem = d % 100
+    d = rem
+    if d >= dial and s == -1 or d >= (100 - dial) and s == 1:
+        res += (1 - dial == 0)
+
+    dial = (dial + s * d) % 100
+
+
+print(res)
+submit(res, part="b", day=day, year=year)
