b58cfeb8e4c015a204134a12cf019cbb74af39b0
carbonara <dylanzam@gmail.com>
2025-12-03 08:57:40 +0100

    day 3 solution

--- day03/part_a.py
+++ day03/part_a.py
@@ -4,8 +4,19 @@ from itertools import combinations
 
 year, day = 2025, 3
 
+dat = '''987654321111111
+811111111111119
+234234234234278
+818181911112111'''
 dat = get_data(year=year, day=day, block=True)
-# CODE HERE
 
+out = 0
+for l in dat.split('\n'):
+    digits = [int(x) for x in l]
+    d1 = max(digits[:-1])
+    idx = digits.index(d1)
+    d2 = max(digits[idx+1:])
+    s = d1*10 +d2
+    out += s
 print(out)
-# submit(out, part="a", day=day, year=year)
+submit(out, part="a", day=day, year=year)
--- day03/part_b.py
+++ day03/part_b.py
@@ -4,8 +4,22 @@ from itertools import combinations
 
 year, day = 2025, 3
 
+dat = '''987654321111111
+811111111111119
+234234234234278
+818181911112111'''
 dat = get_data(year=year, day=day, block=True)
-# CODE HERE
 
+out = 0
+for l in dat.split('\n'):
+    digits = [int(x) for x in l]
+    s, idx, m = 0, 0, 12
+    for p in range(m-1, -1, -1):
+        n = len(digits) if p == 0 else -p
+        available = digits[idx:n]
+        d = max(available)
+        idx = available.index(d) + idx + 1
+        s += d * 10 ** p
+    out += s
 print(out)
-# submit(out, part="b", day=day, year=year)
+submit(out, part="b", day=day, year=year)
