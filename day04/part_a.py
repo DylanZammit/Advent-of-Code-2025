97692f87ece6ba8d3000be62be7640451fa79f88
carbonara <dylanzam@gmail.com>
2025-12-04 23:26:52 +0100

    day 4 solution

--- day04/part_a.py
+++ day04/part_a.py
@@ -5,7 +5,32 @@ from itertools import combinations
 year, day = 2025, 4
 
 dat = get_data(year=year, day=day, block=True)
-# CODE HERE
+dat2 = '''..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.'''
+
+dat = Grid(dat)
+
+out = 0
+for k, v in dat.items():
+    if v == '@':
+        rop = 0
+        success = True
+        for dir in directions8:
+            if dat.get(add2(dir, k)) == '@':
+                rop += 1
+                if rop >= 4:
+                    success = False
+                    break
+        if success:
+            out += 1
 
 print(out)
-# submit(out, part="a", day=day, year=year)
+submit(out, part="a", day=day, year=year)
--- day04/part_b.py
+++ day04/part_b.py
@@ -4,8 +4,37 @@ from itertools import combinations
 
 year, day = 2025, 4
 
+dat = '''..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.'''
+
 dat = get_data(year=year, day=day, block=True)
-# CODE HERE
+dat = Grid(dat)
+
+out = 0
+out_prev = -1
+while out != out_prev:
+    out_prev = out
+    for k, v in dat.items():
+        if v == '@':
+            rop = 0
+            success = True
+            for dir in directions8:
+                if dat.get(add2(dir, k)) == '@':
+                    rop += 1
+                    if rop >= 4:
+                        success = False
+                        break
+            if success:
+                dat[k] = '.'
+                out += 1
 
 print(out)
-# submit(out, part="b", day=day, year=year)
+submit(out, part="b", day=day, year=year)
