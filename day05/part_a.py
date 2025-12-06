112bbe439321996cd8e276dff5c75107107ce00d
carbonara <dylanzam@gmail.com>
2025-12-06 20:40:45 +0100

    day 5 part a sol

--- day05/part_a.py
+++ day05/part_a.py
@@ -4,8 +4,24 @@ from itertools import combinations
 
 year, day = 2025, 5
 
+dat = '''3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32'''
 dat = get_data(year=year, day=day, block=True)
-# CODE HERE
+
+ranges, ing = dat.split('\n\n')
+ranges = [(x, y) for x, y in batched(positive_ints(ranges), 2)]
+ing = positive_ints(ing)
+
+out = len([i for i in ing if any(a <= i <= c for a, c in ranges)])
 
 print(out)
-# submit(out, part="a", day=day, year=year)
+submit(out, part="a", day=day, year=year)
