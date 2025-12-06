f064b2fbae78b0ed99aa3a50dea995c737f7ff9d
carbonara <dylanzam@gmail.com>
2025-12-06 19:22:53 +0100

    day 5 part a sol

--- day06/part_a.py
+++ day06/part_a.py
@@ -1,11 +1,19 @@
 from utils import *
 from aocd import get_data, submit
-from itertools import combinations
+import numpy as np
 
 year, day = 2025, 6
 
+dat = '''123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  '''
 dat = get_data(year=year, day=day, block=True)
-# CODE HERE
 
+eqns, sgns = dat.rsplit('\n', 1)
+eqns = list(zip(*[ints(eqn) for eqn in eqns.split('\n')]))
+sgns = sgns.replace(' ', '')
+
+out = sum(sum(eqn) if op == '+' else np.prod(eqn) for eqn, op in zip(eqns, sgns))
 print(out)
-# submit(out, part="a", day=day, year=year)
+submit(out, part="a", day=day, year=year)
