9bdf25ac42bd666da8369abeeaf4a174b300ec58
carbonara <dylanzam@gmail.com>
2025-12-07 21:31:59 +0100

    day 7 solutions

--- day07/part_a.py
+++ day07/part_a.py
@@ -1,11 +1,53 @@
 from utils import *
 from aocd import get_data, submit
-from itertools import combinations
 
 year, day = 2025, 7
 
+dat = '''.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............'''
 dat = get_data(year=year, day=day, block=True)
-# CODE HERE
 
+dat = Grid(dat)
+
+S = next(k for k, v in dat.items() if v == 'S')
+@cache
+def solve(S):
+    dirn = (0, 1)
+    p = S
+    splits = set()
+    while (new_pos := add2(dirn, p)) in dat:
+
+        if dat[new_pos] in '.|':
+            p = new_pos
+            continue
+
+        splits.add(p)
+        left = add2(new_pos, (-1, 0))
+        right = add2(new_pos, (1, 0))
+
+        if left in dat:
+            splits = splits.union(solve(left))
+        if right in dat:
+            splits.add(right)
+            splits = splits.union(solve(right))
+        break
+    return splits
+
+splits = solve(S)
+out = len(splits) // 2
 print(out)
-# submit(out, part="a", day=day, year=year)
+submit(out, part="a", day=day, year=year)
--- day07/part_b.py
+++ day07/part_b.py
@@ -1,11 +1,50 @@
 from utils import *
 from aocd import get_data, submit
-from itertools import combinations
 
 year, day = 2025, 7
 
+dat = '''.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............'''
 dat = get_data(year=year, day=day, block=True)
-# CODE HERE
 
+dat = Grid(dat)
+
+S = next(k for k, v in dat.items() if v == 'S')
+@cache
+def solve(S):
+    out = 0
+    dirn = (0, 1)
+    p = S
+    while (new_pos := add2(dirn, p)) in dat:
+        if dat[new_pos] in '.':
+            p = new_pos
+            continue
+
+        out += 1
+        left = add2(new_pos, (-1, 0))
+        right = add2(new_pos, (1, 0))
+
+        if left in dat:
+            out += solve(left)
+        if right in dat:
+            out += solve(right)
+        break
+    return out
+
+out = solve(S) + 1
 print(out)
-# submit(out, part="b", day=day, year=year)
+submit(out, part="b", day=day, year=year)
