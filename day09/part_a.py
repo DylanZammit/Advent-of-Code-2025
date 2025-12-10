d5e6160fa788e559d7e34ab12b9a90a87fd59d4e
carbonara <dylanzam@gmail.com>
2025-12-10 08:17:30 +0100

    day 9 solution

--- day09/part_a.py
+++ day09/part_a.py
@@ -4,8 +4,16 @@ from itertools import combinations
 
 year, day = 2025, 9
 
+dat = '''7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3'''
 dat = get_data(year=year, day=day, block=True)
-# CODE HERE
-
+dat = [(x, y) for x, y in batched(ints(dat), 2)]
+out = max(abs(a[0] - b[0] + 1) * abs(a[1] - b[1] + 1) for a, b in combinations(dat, 2))
 print(out)
-# submit(out, part="a", day=day, year=year)
+submit(out, part="a", day=day, year=year)
